16c4aa6d0870bc95b27cf51a30d15b7fe332fd2f
Susannah Waite <susannahwaite2@gmail.com>
2016-10-19 11:18:03 -0500

    get route written and new view file created

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var todolist = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'To Do List' });
+});
+
+router.get('/', function(req, res, next) {
+  res.render(todolist);
 });
 
 module.exports = router;
